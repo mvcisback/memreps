10fa4bf794d5d54a14cae909f9a91444abeb3597
Marcell Vazquez-Chanlatte <mvc@linux.com>
2021-08-06 17:52:59 -0700

    Continue sketching memreps

--- memreps/memreps.py
+++ memreps/memreps.py
@@ -1,15 +1,19 @@
 from __future__ import annotations
 
-from typing import (Any, Callable, Generator, Iterable, Literal,
-                    Protocol, Sequence, TypeVar)
+from functools import partial
+from typing import Any, Callable, Generator, Iterable, Literal, Protocol
 
-from exp4 import exp4
+import attr
+import exp4
 
 
 Atom = Any
 
 
 class Concept(Protocol):
+    def __in__(self, atom: Atom) -> bool:
+        ...
+
     def __xor__(self, other: Concept) -> Concept:
         ...
 
@@ -28,7 +32,7 @@ CmpQuery = tuple[Literal['≺'], tuple[Atom, Atom]]
 EqQuery = tuple[Literal['≡'], Concept]
 Query = MemQuery | CmpQuery | EqQuery
 
-# Types of responses from teacher. 
+# Types of responses from teacher.
 MemResponse = Literal['∈', '∉']
 CmpResponse = Literal['≺', '≻', '=', '||']
 EqResponse = tuple[MemQuery, MemResponse] | tuple[CmpQuery, CmpResponse]
@@ -36,21 +40,18 @@ Response = MemResponse | CmpResponse | EqResponse
 
 LearningAPI = Generator[Query, Response, None]
 
-
 # ================== Concept Class =========================
 
-
 Assumption = EqResponse
-Assumptions = Sequnce[Assumption]
+Assumptions = list[Assumption]
 # Note: Generated concepts should all be unique!
 ConceptClass = Callable[[Assumptions], Iterable[Concept]]
-
 
 # ====================== Learner ===========================
 
 
 def create_learner(
-        find_concept: ConceptClass,
+        gen_concepts: ConceptClass,
         membership_cost: float,
         compare_cost: float) -> LearningAPI:
     """Create learner for interactiving learning a concept φ*.
@@ -58,10 +59,10 @@ def create_learner(
     Assumes that atoms form a membership respecting pre-order, i.e.,
 
         (x ≼ y) ⇒ (x ∈ φ* → y ∈ φ*)
-    
+
     Arguments:
       - membership_cost: Cost of testing if a atom is part of a concept.
-      - compare_cost: Cost of comparing two atoms. 
+      - compare_cost: Cost of comparing two atoms.
 
     Yields:
       One of three kinds of queries:
@@ -82,14 +83,13 @@ def create_learner(
             If φ = φ*, then no return is expected. The query and response
             correspond to membership or comparisons.
     """
-    bandit_alg = exp4()
     assumptions = []
-
-    def choose_query(atom1, atom2) -> Query:
-        pass
+    response = None
+    gen_concepts = partial(gen_concepts, assumptions=assumptions)
+    query_selector = QuerySelector(gen_concepts, membership_cost, compare_cost)
 
     while True:
-        concepts = find_concept(assumptions)
+        concepts = gen_concepts()
 
         if (concept1 := next(concepts, None)) is None:
             return                                         # |Φ| = 0.
@@ -99,11 +99,52 @@ def create_learner(
         else:
             concept12 = concept1 ^ concept2
             atom1, atom2 = next(concept12), next(~concept12)
-            query = choose_query(atom1, atom2)
+
+            queries = [('∈', atom1), ('≺', (atom1, atom2))]
+            query = query_selector(queries)
 
         response = yield query
         
         if query[0] != '≡':  # Equiv responses contain query already. 
+            query_selector.update(response)
             response = (query, response)
  
         assumptions.append(response)
+
+
+# ===================== Bandit Details =====================
+
+# TODO: implement experts.
+EXPERTS = []
+
+
+@attr.s(auto_detect=True, auto_attribs=True)
+class QuerySelector:
+    gen_concepts: ConceptClass
+    mem_cost: float
+    cmp_cost: float
+
+    # Internal State
+    player: exp4.Player = attr.ib(factory=exp4.exp4)
+    loss: float | None = None
+    loss_map: dict[MemResponse | CmpResponse, float] = attr.ib(factory=dict)
+
+    def __call__(self, queries: list[Query]) -> Query:
+        summaries = [self.summarize(q) for q in queries]
+        advice = [expert(summary) for expert in EXPERTS]
+
+        arm = self.player.send((self.loss, advice))
+        query = queries[arm]
+
+        # Setup loss_map
+        query_cost = self.mem_cost if query[0] == '∈' else self.cmp_cost
+        query_cost /= max(self.mem_cost, self.cmp_cost)
+        self.loss_map = {k: (query_cost + s) / 2 for k, s in summaries[arm]}
+
+        return query
+
+    def update(self, response: MemResponse | CmpResponse):
+        self.loss = self.loss_map[response]
+
+    def summarize(self) -> dict[MemResponse | CmpResponse, float]:
+        ...
