37140a797c8dde61c9daa7bd31179636588953b2
vint <vint@berkeley.edu>
2021-08-28 01:00:29 -0700

    added seperating hyperplane generator

--- memreps/grid.py
+++ memreps/grid.py
@@ -32,3 +32,39 @@ def create_grid_concept_class(
     concepts = (family(p) for p in product(*repeat(points1d, dim)))
 
     return create_implicit_concept_class(elems, concepts)
+
+def sep_hyperplane(p1: np.ndarray, p2: np.ndarray) -> np.ndarray:
+
+    """
+    Given 2 points, p1 and p2, generate a separating hyperplane for the two
+    To extract p1 and p2, we have to go into Concept, extract Predicate, and look at Parameters 
+    
+    Hyperplane is represented as: x_0 = a_1*x_1 + a_2*x_2 + ... + a_n, where a_i are float parameters
+    Constraints:
+        - all gradients (ie parameters other than a_n) must be negative
+        - hyperplane must be separating
+
+    output:
+    ndarray containing a_1, a_2, ...
+    """
+    limit = 10.0
+    eta = 1e-12
+    dim = p1.shape[0]
+    
+    A = np.zeros((dim))
+    h1 = 0
+    h2 = 0
+    while abs(h1 - h2) < eta:
+        # Generating random gradients (a_n = 0 for now)
+        A[:-1] = -np.random.default_rng().uniform(0, limit, size = (dim-1))
+
+        # Check if separating hyperplane is possible with generated gradients
+        # We do this by calculating x_0 of hyperplane at p1 and p2's locations
+        # These must differ by at least eta, for there to be some a_n that separates the two
+        h1 = np.dot(p1[1:], A[:-1])
+        h2 = np.dot(p2[1:], A[:-1])
+
+    delta1 = p1[0] - h1
+    delta2 = p2[0] - h2
+    A[-1] = np.random.default_rng().uniform(min(delta1, delta2), max(delta1, delta2))
+    return A
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,5 +1,6 @@
 import attr
 import numpy as np
+import matplotlib.pyplot as plt
 
 from memreps import memreps
 from memreps import grid
@@ -38,3 +39,40 @@ def test_simple_grid_concept_class():
     assumptions = [(('≺', (left, right)), '≻')]
 
     assert set(gen_concepts(assumptions)) < set(gen_concepts())
+
+def test_hyperplane():
+    # 3D testing
+    dim = 3
+    for _ in range(10):
+        p1 = np.random.default_rng().uniform(0, 1, size = (dim))
+        p2 = np.random.default_rng().uniform(0, 1, size = (dim))
+        plane = grid.sep_hyperplane(p1, p2)
+
+        def h(x, p):
+            return np.dot(x, p[:-1]) + p[-1]
+
+        # print(p1, p2, plane)
+        # checks whether hyperplane is really separating
+        h1 = h(p1[1:], plane) - p1[0]
+        h2 = h(p2[1:], plane) - p2[0]
+        # h1 = np.dot(p1[1:], plane[:-1]) + plane[-1] - p1[0]
+        # h2 = np.dot(p2[1:], plane[:-1]) + plane[-1] - p2[0]
+        assert h1 * h2 < 0
+
+    print(p1, p2, plane)
+    print(h1, h2)
+
+    # # plotting stuff
+    # points = np.stack((p1, p2))
+    # x = np.arange(0, 1.1, 0.2)
+    # y = np.arange(0, 1.1, 0.2)
+    # x, y = np.meshgrid(x, y)
+    # z = np.reshape(h(np.stack((np.reshape(x,-1),np.reshape(y,-1))).T, plane), x.shape)
+    # ax = plt.axes(projection = "3d")
+    # ax.scatter3D(points[:,1], points[:,2], points[:,0])
+    # ax.plot_surface(x, y, z)
+    # ax.set_title(plane)
+    # ax.set_xlabel("x")
+    # ax.set_ylabel("y")
+    # ax.set_zlabel("z")
+    # plt.show()
