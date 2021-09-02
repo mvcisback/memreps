7a3a435f897ddb76234f0f932626d843332ea30f
Marcell Vazquez-Chanlatte <mvc@linux.com>
2021-09-02 15:20:03 -0700

    Use size indexed concept class.

--- tests/test_monotone.py
+++ tests/test_monotone.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from itertools import combinations
 
 import numpy as np
@@ -55,45 +56,70 @@ def test_monotone_grid():
 
 
 def test_monotone_memreps():
+    res = 4 
+    my_param = np.array((1/4, 2/4))
+    my_concept = MonotoneConcept.from_point(my_param)
+    ticks = 3
+
+    def my_concept_class(assumptions):
+        nonlocal ticks
+
+        for ticks in range(ticks, 6):
+            concepts = create_monotone_concept_class(dim=2, num_ticks=ticks)
+            concepts = concepts(assumptions)
+
+            if (first := fn.first(concepts)) is not None:
+                yield first
+                yield from concepts
+                break
+
     learner = memreps.create_learner(
-        create_monotone_concept_class(dim=2, num_ticks=5),
-        membership_cost=100,
-        compare_cost=1,
-        query_limit=200
+        my_concept_class,
+        compare_cost=200,
+        membership_cost=1,
+        query_limit=200,
     )
 
-    res = 4 
-    my_concept = MonotoneConcept.from_point((1/4, 2/4))
-
     response = None
+    query_histogram = Counter()
     for count in range(100):
         kind, payload = learner.send(response)
+        query_histogram.update(kind)
 
         if kind == '≺':
             left, right = map(np.array, payload)
-            left_in, right_in = (x in my_concept for x in payload)
 
             intersects_l = np.array([left[1], -left[1] / left[0]])
             intersects_r = np.array([right[1], -right[1] / right[0]])
 
-            #if (intersects_l < intersects_r).all():
-            if left_in < right_in:
+            if (intersects_l < intersects_r).all():
                 assert (left in my_concept) <= (right in my_concept)
                 response = '≺'
-            #elif (intersects_r < intersects_l).all():
-            elif left_in > right_in:
+            elif (intersects_r < intersects_l).all():
                 assert (right in my_concept) <= (left in my_concept)
                 response = '≻'
-            elif left_in == right_in:
-            #elif (intersects_l == intersects_r).all():
+            elif (intersects_l == intersects_r).all():
                 assert (left in my_concept) == (right in my_concept)
                 response = '='
             else:
                 response = '||'
+
+            print('here', response)
         elif kind == '≡':
-            assert my_concept == payload
-            break
+            if my_concept == payload:
+                break
+            else:
+                point = np.array(fn.first(payload.points)) + my_param
+                point /= 2
+                pertubed_concept = MonotoneConcept.from_point(point)
+                plane = fn.first(pertubed_concept ^ my_concept)
+                mem_response = '∈' if plane in my_concept else '∉'
+                response = ('∈', plane), mem_response
         else:
             assert kind == '∈'
             response = '∈' if payload in my_concept else '∉'
+
+    assert my_concept == payload
+    assert count < 20
+    print(query_histogram)
     print(count)
