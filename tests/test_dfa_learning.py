d781a8ecf125f275ff37bb5a9b1ca1d84f3b13dd
ameesh <ars7@rice.edu>
2022-01-06 18:48:56 -0800

    adding tomita languages

--- tests/test_dfa_learning.py
+++ tests/test_dfa_learning.py
@@ -4,24 +4,53 @@ from dfa import dict2dfa
 from memreps.dfa_learning import dfa_memreps, create_dfa_concept_class
 import numpy as np
 
-def test_equivalence_memreps():
-    def oracle(full_query):
-        query_symbol, query = full_query
-        if query_symbol == '∈':
-            # mem query case
-            return membership_fxn(query)
-        elif query_symbol == '≺':
-            # cmp query case
-            return pref_fxn(query[0], query[1])
-        else:
-            # eq query case
-            resp = equivalence_fxn(query.dfa)
-            if resp is None:
-                return None
-            else:
-                counterex, label = resp
-                return ('∈', counterex), label
+'''
+memreps boilerplate functions
+'''
+def pref_fxn(word1, word2, true_dfa):
+    if true_dfa.label(word1) == true_dfa.label(word2):
+        return '||'
+    elif true_dfa.label(word1):
+        return '≻'
+    else:
+        return '≺'
 
+
+def membership_fxn(word, true_dfa):
+    if true_dfa.label(word):
+        return '∈'
+    else:
+        return '∉'
+
+
+def equivalence_fxn(candidate, true_dfa):
+    cex = find_equiv_counterexample(candidate, true_dfa)
+    if cex is None:
+        return None
+    else:
+        if true_dfa.label(cex):
+            return cex, '∈'
+        else:
+            return cex, '∉'
+
+def oracle(full_query, true_dfa):
+    query_symbol, query = full_query
+    if query_symbol == '∈':
+        # mem query case
+        return membership_fxn(query, true_dfa)
+    elif query_symbol == '≺':
+        # cmp query case
+        return pref_fxn(query[0], query[1], true_dfa)
+    else:
+        # eq query case
+        resp = equivalence_fxn(query.dfa, true_dfa)
+        if resp is None:
+            return None
+        else:
+            counterex, label = resp
+            return ('∈', counterex), label
+
+def test_equivalence_memreps():
     transition_dict = {0: (True, {'a': 1, 'b': 0}),
                        1: (True, {'a' : 4, 'b': 2}),
                        2: (True, {'a' : 5, 'b': 3}),
@@ -31,54 +60,17 @@ def test_equivalence_memreps():
 
     true_dfa = dict2dfa(transition_dict, 0)
 
-    def pref_fxn(word1, word2):
-        if true_dfa.label(word1) == true_dfa.label(word2):
-            return '||'
-        elif true_dfa.label(word1):
-            return '≻'
-        else:
-            return '≺'
-
-    def membership_fxn(word):
-        if true_dfa.label(word):
-            return '∈'
-        else:
-            return '∉'
-
-    def equivalence_fxn(candidate):
-        cex = find_equiv_counterexample(candidate, true_dfa)
-        if cex is None:
-            return None
-        else:
-            if true_dfa.label(cex):
-                return cex, '∈'
-            else:
-                return cex, '∉'
+    def oracle_wrapper(full_query):
+        return oracle(full_query, true_dfa)
 
     accepting = ['b', 'aa', 'a']
     rejecting = ['aaaaa', 'abb']
 
-    resulting_dfa = dfa_memreps(oracle, 2, 1, accepting=accepting, rejecting=rejecting)
+    resulting_dfa, query_histogram = dfa_memreps(oracle_wrapper, 2, 1, accepting=accepting, rejecting=rejecting)
     assert find_equiv_counterexample(true_dfa, resulting_dfa.dfa) is None
 
 
 def test_gridworld_dfa(membership_cost: int = 1, force_membership: bool = False):
-    def oracle(full_query):
-        query_symbol, query = full_query
-        if query_symbol == '∈':
-            # mem query case
-            return membership_fxn(query)
-        elif query_symbol == '≺':
-            # cmp query case
-            return pref_fxn(query[0], query[1])
-        else:
-            # eq query case
-            resp = equivalence_fxn(query.dfa)
-            if resp is None:
-                return None
-            else:
-                counterex, label = resp
-                return ('∈', counterex), label
     transition_dict = {0: (False, {'R': 0, 'B': 0, 'O': 0, 'Y': 0}),
                        1: (False, {'R': 0, 'B': 1, 'O': 2, 'Y': 0}),
                        2: (False, {'R': 0, 'B': 1, 'O': 2, 'Y': 3}),
@@ -87,38 +79,61 @@ def test_gridworld_dfa(membership_cost: int = 1, force_membership: bool = False)
 
     true_dfa = dict2dfa(transition_dict, 2)
 
-    def pref_fxn(word1, word2):
-        if np.random.rand() < 0.1:
-            response = '||'
-
-        left, right = true_dfa.label(word1), true_dfa.label(word2)
-        if left < right:
-            return '≺'
-        elif right < left:
-            return '≻'
-        else:
-            return '=' if np.random.rand() > 0.3 else '≺'
-
-    def membership_fxn(word):
-        if true_dfa.label(word):
-            return '∈'
-        else:
-            return '∉'
-
-    def equivalence_fxn(candidate):
-        cex = find_equiv_counterexample(candidate, true_dfa)
-        if cex is None:
-            return None
-        else:
-            if true_dfa.label(cex):
-                return cex, '∈'
-            else:
-                return cex, '∉'
+    def oracle_wrapper(full_query):
+        return oracle(full_query, true_dfa)
 
     accepting = ['Y', 'YY', 'OY']
     rejecting = ['', 'R', 'RY', 'YR', 'BR', 'RB', 'OR', 'RO']
 
-    resulting_dfa, query_hist = dfa_memreps(oracle, membership_cost, 1, query_limit=250,
+    resulting_dfa, query_hist = dfa_memreps(oracle_wrapper, membership_cost, 1, query_limit=250,
                                             accepting=accepting, rejecting=rejecting, force_membership=force_membership)
     assert find_equiv_counterexample(true_dfa, resulting_dfa.dfa) is None
     return query_hist
+
+
+tomita_transition_dicts = [
+    {0: (True, {"1": 0, "0": 1}),
+     1: (False, {"1": 1, "0": 1})},
+
+    {0: (True, {"1": 1, "0": 2}),
+     1: (False, {"1": 2, "0": 0}),
+     2: (False, {"1": 2, "0": 2})},
+
+    {0: (True, {"0": 0, "1": 1}),
+     1: (True, {"0": 2, "1": 0}),
+     2: (False, {"0": 3, "1": 4}),
+     3: (False, {"0": 2, "1": 3}),
+     4: (False, {"0": 4, "1": 4})},
+
+    {0: (True, {"0": 1, "1": 0}),
+     1: (True, {"0": 2, "1": 0}),
+     2: (True, {"0": 3, "1": 0}),
+     3: (False, {"0": 3, "1": 3})},
+
+    {0: (True, {"0": 3, "1": 1}),
+     1: (False, {"0": 2, "1": 0}),
+     2: (False, {"0": 1, "1": 3}),
+     3: (False, {"0": 0, "1": 2})},
+
+    {0: (True, {"0": 2, "1": 1}),
+     1: (False, {"0": 0, "1": 2}),
+     2: (False, {"0": 1, "1": 0})},
+
+    {0: (True, {"0": 0, "1": 1}),
+     1: (True, {"0": 2, "1": 1}),
+     2: (True, {"0": 2, "1": 3}),
+     3: (True, {"0": 4, "1": 3}),
+     4: (False, {"0": 4, "1": 4})}
+]
+
+def test_tomita(tomita_dict_idx = 0, membership_cost: int = 1, force_membership: bool = False):
+    true_dfa = dict2dfa(tomita_transition_dicts[tomita_dict_idx], 0)
+    accepting = []
+    rejecting = []
+    def oracle_wrapper(full_query):
+        return oracle(full_query, true_dfa)
+    resulting_dfa, query_hist = dfa_memreps(oracle_wrapper, membership_cost, 1, query_limit=250,
+                                            accepting=accepting, rejecting=rejecting, force_membership=force_membership)
+    assert find_equiv_counterexample(true_dfa, resulting_dfa.dfa) is None
+    return query_hist
+
